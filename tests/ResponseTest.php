b830e7d35f15f132790e158bb209ecc17616bbe2
Robert Allport <rob@f9web.co.uk>
2026-05-31 10:04:19 +0100

    Refactor data providers, add test cases
    - refactor data providers to use generators
    - add missing tests for new helper methods

--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -60,334 +60,352 @@ class ResponseTest extends TestCase
         self::assertJsonStringEqualsJsonString(json_encode($data, JSON_THROW_ON_ERROR), $response->getContent());
     }
 
-    public static function basicResponsesDataProvider(): array
+    public static function basicResponsesDataProvider(): iterable
     {
-        return [
-          'respondNotFound()' => [
-            'respondNotFound',
-            ['Ouch'],
-            Response::HTTP_NOT_FOUND,
-            ['error' => 'Ouch'],
-          ],
+		yield 'respondNotFound()' => [
+			'respondNotFound',
+			['Ouch'],
+			Response::HTTP_NOT_FOUND,
+			['error' => 'Ouch'],
+		];
+		
+		yield 'respondNotFound() with custom key' => [
+			'respondNotFound',
+			['Ouch', 'message'],
+			Response::HTTP_NOT_FOUND,
+			['message' => 'Ouch'],
+		];
+		
+		yield 'respondNotFound() with exception and custom key' => [
+			'respondNotFound',
+			[new DomainException('Unknown model'), 'message'],
+			Response::HTTP_NOT_FOUND,
+			['message' => 'Unknown model'],
+		];
+		
+		yield 'respondWithSuccess(), default response data' => [
+			'respondWithSuccess',
+			[],
+			Response::HTTP_OK,
+			['success' => true],
+		];
+		
+		yield 'respondWithSuccess(), null response data' => [
+			'respondWithSuccess',
+			[null],
+			Response::HTTP_OK,
+			['success' => true],
+		];
+		
+		yield 'respondWithSuccess(), custom response data' => [
+			'respondWithSuccess',
+			[['order' => 237]],
+			Response::HTTP_OK,
+			['order' => 237],
+		];
+		
+		yield 'respondWithSuccess(), nested custom response data' => [
+			'respondWithSuccess',
+			[['order' => 237, 'customer' => ['name' => 'Jason Bourne']]],
+			Response::HTTP_OK,
+			['order' => 237, 'customer' => ['name' => 'Jason Bourne']],
+		];
+		
+		yield 'respondWithSuccess(), collection' => [
+			'respondWithSuccess',
+			[new Collection(['invoice' => 23, 'status' => 'pending'])],
+			Response::HTTP_OK,
+			['invoice' => 23, 'status' => 'pending'],
+		];
+		
+		yield 'respondWithSuccess(), empty collection' => [
+			'respondWithSuccess',
+			[new Collection()],
+			Response::HTTP_OK,
+			['success' => true],
+		],
+		
+		yield 'respondWithSuccess(), Arrayable' => [
+			'respondWithSuccess',
+			[
+				new class implements Arrayable {
+					public function toArray() { return ['id' => 1, 'name' => 'John']; }
+				},
+			],
+			Response::HTTP_OK,
+			['id' => 1, 'name' => 'John']
+		];
+		
+		yield 'respondWithSuccess(), empty Arrayable' => [
+			'respondWithSuccess',
+			[
+				new class implements Arrayable {
+					public function toArray() { return []; }
+				},
+			],
+			Response::HTTP_OK,
+			['success' => true]
+		];
+		
+		yield 'respondWithSuccess(), JsonSerializable' => [
+		'respondWithSuccess',
+			[
+				new class implements \JsonSerializable {
+					public function jsonSerialize() { return ['id' => 1, 'name' => 'John']; }
+				},
+			],
+			Response::HTTP_OK,
+			['id' => 1, 'name' => 'John']
+		];
+		
+		yield 'respondWithSuccess(), empty JsonSerializable' => [
+			'respondWithSuccess',
+			[
+				new class implements \JsonSerializable {
+					public function jsonSerialize() { return []; }
+				},
+			],
+			Response::HTTP_OK,
+			['success' => true]
+		];
+		
+		yield 'respondOk()' => [
+			'respondOk',
+			['Order accepted'],
+			Response::HTTP_OK,
+			['success' => 'Order accepted'],
+		];
+		
+		yield 'respondUnAuthenticated(), default message' => [
+			'respondUnAuthenticated',
+			[],
+			Response::HTTP_UNAUTHORIZED,
+			['error' => 'Unauthenticated'],
+		];
+		
+		yield 'respondUnAuthenticated(), custom message' => [
+			'respondUnAuthenticated',
+			['Denied'],
+			Response::HTTP_UNAUTHORIZED,
+			['error' => 'Denied'],
+		];
+		
+		yield 'respondForbidden(), default message' => [
+			'respondForbidden',
+			[],
+			Response::HTTP_FORBIDDEN,
+			['error' => 'Forbidden'],
+		];
+		
+		yield 'respondForbidden(), custom message' => [
+			'respondForbidden',
+			['Disavowed'],
+			Response::HTTP_FORBIDDEN,
+			['error' => 'Disavowed'],
+		];
+		
+		yield 'respondError(), default message' => [
+			'respondError',
+			[],
+			Response::HTTP_BAD_REQUEST,
+			['error' => 'Error'],
+		];
+		
+		yield 'respondError(), custom message' => [
+			'respondError',
+			['Order tampering detected'],
+			Response::HTTP_BAD_REQUEST,
+			['error' => 'Order tampering detected'],
+		];
+		
+		yield 'respondCreated()' => [
+			'respondCreated',
+			[],
+			Response::HTTP_CREATED,
+			[],
+		];
+		
+		yield 'respondCreated() with null' => [
+			'respondCreated',
+			[null],
+			Response::HTTP_CREATED,
+			[],
+		];
+		
+		yield 'respondCreated() with response data' => [
+			'respondCreated',
+			[['user' => ['name' => 'Jet Li']]],
+			Response::HTTP_CREATED,
+			['user' => ['name' => 'Jet Li']],
+		];
+		
+		yield 'respondCreated(), with collection as response' => [
+			'respondCreated',
+			[new Collection(['invoice' => 23, 'status' => 'pending'])],
+			Response::HTTP_CREATED,
+			['invoice' => 23, 'status' => 'pending'],
+		];
+		
+		yield 'respondCreated(), with eloquent collection as response' => [
+			'respondCreated',
+			[
+				new EloquentCollection([
+					new User(['name' => 'Jet Li', 'age' => 58]),
+					new User(['name' => 'Chow Yun-Fat', 'age' => 66]),
+					new User(['name' => 'Donnie Yen', 'age' => 58])
+				])
+			],
+			Response::HTTP_CREATED,
+			[
+				['name' => 'Jet Li', 'age' => 58],
+				['name' => 'Chow Yun-Fat', 'age' => 66],
+				['name' => 'Donnie Yen', 'age' => 58]
+			],
+		];
+		
+		yield 'respondCreated(), with json resource as response' => [
+			'respondCreated',
+			[
+				new UserResource(new User(['name' => 'Jet Li', 'age' => 58]))
+			],
+			Response::HTTP_CREATED,
+			['nameAndAge' => 'Jet Li & 58'],
+		];
+		
+		yield 'respondCreated(), with resource collection as response' => [
+			'respondCreated',
+			[
+				UserResource::collection(
+					new EloquentCollection([
+						new User(['name' => 'Jet Li', 'age' => 58]),
+						new User(['name' => 'Chow Yun-Fat', 'age' => 66]),
+						new User(['name' => 'Donnie Yen', 'age' => 58])
+					])
+				)
+			],
+			Response::HTTP_CREATED,
+			[
+				['nameAndAge' => 'Jet Li & 58'],
+				['nameAndAge' => 'Chow Yun-Fat & 66'],
+				['nameAndAge' => 'Donnie Yen & 58']
+			],
+		];
+		
+		yield 'respondFailedValidation()' => [
+			'respondFailedValidation',
+			['An invoice is required'],
+			Response::HTTP_UNPROCESSABLE_ENTITY,
+			['message' => 'An invoice is required'],
+		];
+		
+		yield 'respondTeapot()' => [
+			'respondTeapot',
+			[],
+			Response::HTTP_I_AM_A_TEAPOT,
+			['message' => 'I\'m a teapot'],
+		];
+		
+		yield 'respondNoContent()' => [
+			'respondNoContent',
+			[],
+			Response::HTTP_NO_CONTENT,
+			[],
+		];
+		
+		yield 'respondNoContent() with null' => [
+			'respondNoContent',
+			[null],
+			Response::HTTP_NO_CONTENT,
+			[],
+		];
+		
+		// @see https://github.com/f9webltd/laravel-api-response-helpers/issues/5#issuecomment-917418285
+		yield 'respondNoContent() with data' => [
+			'respondNoContent',
+			[['role' => 'manager']],
+			Response::HTTP_NO_CONTENT,
+			['role' => 'manager'],
+		];
+		
+		// @see https://github.com/f9webltd/laravel-api-response-helpers/issues/5#issuecomment-917418285
+		yield 'respondNoContent(), with collection as response' => [
+			'respondNoContent',
+			[new Collection(['invoice' => 23, 'status' => 'pending'])],
+			Response::HTTP_NO_CONTENT,
+			['invoice' => 23, 'status' => 'pending'],
+		];
 
-          'respondNotFound() with custom key' => [
-            'respondNotFound',
-            ['Ouch', 'message'],
-            Response::HTTP_NOT_FOUND,
-            ['message' => 'Ouch'],
-          ],
+		yield 'respondAccepted() with data' => [
+			'respondAccepted',
+			[['role' => 'manager']],
+			Response::HTTP_ACCEPTED,
+			[],
+		];
 
-          'respondNotFound() with exception and custom key' => [
-            'respondNotFound',
-            [
-              new DomainException('Unknown model'),
-              'message'
-            ],
-            Response::HTTP_NOT_FOUND,
-            ['message' => 'Unknown model'],
-          ],
+		yield 'respondConflict(), no message' => [
+			'respondConflict',
+			[null],
+			Response::HTTP_CONFLICT,
+			['error' => 'Conflict'],
+		];
 
-          'respondWithSuccess(), default response data' => [
-            'respondWithSuccess',
-            [],
-            Response::HTTP_OK,
-            ['success' => true],
-          ],
+		yield 'respondConflict(), custom message' => [
+			'respondConflict',
+			['Hmmm, conflicted ...'],
+			Response::HTTP_CONFLICT,
+			['error' => 'Hmmm, conflicted ...'],
+		];
 
-          'respondWithSuccess(), null response data' => [
-            'respondWithSuccess',
-            [null],
-            Response::HTTP_OK,
-            ['success' => true],
-          ],
+		yield 'respondTooManyRequests(), no message' => [
+			'respondTooManyRequests',
+			[null],
+			Response::HTTP_TOO_MANY_REQUESTS,
+			['error' => 'Too Many Requests'],
+		];
 
-          'respondWithSuccess(), custom response data' => [
-            'respondWithSuccess',
-            [['order' => 237]],
-            Response::HTTP_OK,
-            ['order' => 237],
-          ],
-
-          'respondWithSuccess(), nested custom response data' => [
-            'respondWithSuccess',
-            [['order' => 237, 'customer' => ['name' => 'Jason Bourne']]],
-            Response::HTTP_OK,
-            ['order' => 237, 'customer' => ['name' => 'Jason Bourne']],
-          ],
-
-          'respondWithSuccess(), collection' => [
-            'respondWithSuccess',
-            [new Collection(['invoice' => 23, 'status' => 'pending'])],
-            Response::HTTP_OK,
-            ['invoice' => 23, 'status' => 'pending'],
-          ],
-
-          'respondWithSuccess(), empty collection' => [
-            'respondWithSuccess',
-            [new Collection()],
-            Response::HTTP_OK,
-            ['success' => true],
-          ],
-
-          'respondWithSuccess(), Arrayable' => [
-            'respondWithSuccess',
-            [
-              new class implements Arrayable {
-                public function toArray()
-                {
-                  return ['id' => 1, 'name' => 'John'];
-                }
-              },
-            ],
-            Response::HTTP_OK,
-            ['id' => 1, 'name' => 'John']
-          ],
-
-          'respondWithSuccess(), empty Arrayable' => [
-            'respondWithSuccess',
-            [
-              new class implements Arrayable {
-                public function toArray()
-                {
-                  return [];
-                }
-              },
-            ],
-            Response::HTTP_OK,
-            ['success' => true]
-          ],
-
-          'respondWithSuccess(), JsonSerializable' => [
-            'respondWithSuccess',
-            [
-              new class implements \JsonSerializable {
-                public function jsonSerialize()
-                {
-                  return ['id' => 1, 'name' => 'John'];
-                }
-              },
-            ],
-            Response::HTTP_OK,
-            ['id' => 1, 'name' => 'John']
-          ],
-
-          'respondWithSuccess(), empty JsonSerializable' => [
-            'respondWithSuccess',
-            [
-              new class implements \JsonSerializable {
-                public function jsonSerialize()
-                {
-                  return [];
-                }
-              },
-            ],
-            Response::HTTP_OK,
-            ['success' => true]
-          ],
-
-          'respondOk()' => [
-            'respondOk',
-            ['Order accepted'],
-            Response::HTTP_OK,
-            ['success' => 'Order accepted'],
-          ],
-
-          'respondUnAuthenticated(), default message' => [
-            'respondUnAuthenticated',
-            [],
-            Response::HTTP_UNAUTHORIZED,
-            ['error' => 'Unauthenticated'],
-          ],
-
-          'respondUnAuthenticated(), custom message' => [
-            'respondUnAuthenticated',
-            ['Denied'],
-            Response::HTTP_UNAUTHORIZED,
-            ['error' => 'Denied'],
-          ],
-
-          'respondForbidden(), default message' => [
-            'respondForbidden',
-            [],
-            Response::HTTP_FORBIDDEN,
-            ['error' => 'Forbidden'],
-          ],
-
-          'respondForbidden(), custom message' => [
-            'respondForbidden',
-            ['Disavowed'],
-            Response::HTTP_FORBIDDEN,
-            ['error' => 'Disavowed'],
-          ],
-
-          'respondError(), default message' => [
-            'respondError',
-            [],
-            Response::HTTP_BAD_REQUEST,
-            ['error' => 'Error'],
-          ],
-
-          'respondError(), custom message' => [
-            'respondError',
-            ['Order tampering detected'],
-            Response::HTTP_BAD_REQUEST,
-            ['error' => 'Order tampering detected'],
-          ],
-
-          'respondCreated()' => [
-            'respondCreated',
-            [],
-            Response::HTTP_CREATED,
-            [],
-          ],
-
-          'respondCreated() with null' => [
-            'respondCreated',
-            [null],
-            Response::HTTP_CREATED,
-            [],
-          ],
-
-          'respondCreated() with response data' => [
-            'respondCreated',
-            [['user' => ['name' => 'Jet Li']]],
-            Response::HTTP_CREATED,
-            ['user' => ['name' => 'Jet Li']],
-          ],
-
-          'respondCreated(), with collection as response' => [
-            'respondCreated',
-            [new Collection(['invoice' => 23, 'status' => 'pending'])],
-            Response::HTTP_CREATED,
-            ['invoice' => 23, 'status' => 'pending'],
-          ],
-
-          'respondCreated(), with eloquent collection as response' => [
-            'respondCreated',
-            [
-              new EloquentCollection([
-                new User(['name' => 'Jet Li', 'age' => 58]),
-                new User(['name' => 'Chow Yun-Fat', 'age' => 66]),
-                new User(['name' => 'Donnie Yen', 'age' => 58])
-              ])
-            ],
-            Response::HTTP_CREATED,
-            [
-              ['name' => 'Jet Li', 'age' => 58],
-              ['name' => 'Chow Yun-Fat', 'age' => 66],
-              ['name' => 'Donnie Yen', 'age' => 58]
-            ],
-          ],
-
-          'respondCreated(), with json resource as response' => [
-            'respondCreated',
-            [
-              new UserResource(
-                new User(['name' => 'Jet Li', 'age' => 58])
-              )
-            ],
-            Response::HTTP_CREATED,
-            ['nameAndAge' => 'Jet Li & 58'],
-          ],
-
-          'respondCreated(), with resource collection as response' => [
-            'respondCreated',
-            [
-              UserResource::collection(
-                new EloquentCollection([
-                  new User(['name' => 'Jet Li', 'age' => 58]),
-                  new User(['name' => 'Chow Yun-Fat', 'age' => 66]),
-                  new User(['name' => 'Donnie Yen', 'age' => 58])
-                ])
-              )
-            ],
-            Response::HTTP_CREATED,
-            [
-              ['nameAndAge' => 'Jet Li & 58'],
-              ['nameAndAge' => 'Chow Yun-Fat & 66'],
-              ['nameAndAge' => 'Donnie Yen & 58']
-            ],
-          ],
-
-          'respondFailedValidation()' => [
-            'respondFailedValidation',
-            ['An invoice is required'],
-            Response::HTTP_UNPROCESSABLE_ENTITY,
-            ['message' => 'An invoice is required'],
-          ],
-
-          'respondTeapot()' => [
-            'respondTeapot',
-            [],
-            Response::HTTP_I_AM_A_TEAPOT,
-            ['message' => 'I\'m a teapot'],
-          ],
-
-          'respondNoContent()' => [
-            'respondNoContent',
-            [],
-            Response::HTTP_NO_CONTENT,
-            [],
-          ],
-
-          'respondNoContent() with null' => [
-            'respondNoContent',
-            [null],
-            Response::HTTP_NO_CONTENT,
-            [],
-          ],
-
-          // @see https://github.com/f9webltd/laravel-api-response-helpers/issues/5#issuecomment-917418285
-          'respondNoContent() with data' => [
-            'respondNoContent',
-            [['role' => 'manager']],
-            Response::HTTP_NO_CONTENT,
-            ['role' => 'manager'],
-          ],
-
-          // @see https://github.com/f9webltd/laravel-api-response-helpers/issues/5#issuecomment-917418285
-          'respondNoContent(), with collection as response' => [
-            'respondNoContent',
-            [new Collection(['invoice' => 23, 'status' => 'pending'])],
-            Response::HTTP_NO_CONTENT,
-            ['invoice' => 23, 'status' => 'pending'],
-          ],
-        ];
+		yield 'respondTooManyRequests(), custom message' => [
+			'respondTooManyRequests',
+			['Spamming ...'],
+			Response::HTTP_TOO_MANY_REQUESTS,
+			['error' => 'Spamming ...'],
+		];
     }
 
-    public static function successDefaultsDataProvider(): array
+    public static function successDefaultsDataProvider(): iterable
     {
-        return [
-            'respondWithSuccess(), default empty array' => [
-                'default' => [],
-                'args' => [],
-                'code' => Response::HTTP_OK,
-                'data' => [],
-            ],
-            'respondWithSuccess(), default null' => [
-                'default' => null,
-                'args' => [],
-                'code' => Response::HTTP_OK,
-                'data' => [],
-            ],
-            'respondWithSuccess(), default null, null response' => [
-                'default' => null,
-                'args' => null,
-                'code' => Response::HTTP_OK,
-                'data' => [],
-            ],
-            'respondWithSuccess(), default non-empty array' => [
-                'default' => ['message' => 'Task successful!'],
-                'args' => [],
-                'code' => Response::HTTP_OK,
-                'data' => ['message' => 'Task successful!'],
-            ],
-            'respondWithSuccess(), default non-empty array, custom response data' => [
-                'default' => ['message' => 'Task successful!'],
-                'args' => ['numbers' => [1, 2, 3]],
-                'code' => Response::HTTP_OK,
-                'data' => ['numbers' => [1, 2, 3]],
-            ]
+        yield 'respondWithSuccess(), default empty array' => [
+            'default' => [],
+            'args' => [],
+            'code' => Response::HTTP_OK,
+            'data' => [],
+        ];
+
+        yield 'respondWithSuccess(), default null' => [
+            'default' => null,
+            'args' => [],
+            'code' => Response::HTTP_OK,
+            'data' => [],
+        ];
+
+        yield 'respondWithSuccess(), default null, null response' => [
+            'default' => null,
+            'args' => null,
+            'code' => Response::HTTP_OK,
+            'data' => [],
+        ];
+
+        yield 'respondWithSuccess(), default non-empty array' => [
+            'default' => ['message' => 'Task successful!'],
+            'args' => [],
+            'code' => Response::HTTP_OK,
+            'data' => ['message' => 'Task successful!'],
+        ];
+
+        yield 'respondWithSuccess(), default non-empty array, custom response data' => [
+            'default' => ['message' => 'Task successful!'],
+            'args' => ['numbers' => [1, 2, 3]],
+            'code' => Response::HTTP_OK,
+            'data' => ['numbers' => [1, 2, 3]],
         ];
     }
 }
